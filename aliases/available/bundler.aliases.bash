f4a33c4e8550d66b7631ed695483263e09c5dbbf
Peter Kananen <pkananen@gmail.com>
2011-10-07 10:34:26 -0400

    bundler aliases

--- aliases/available/bundler.aliases.bash
+++ aliases/available/bundler.aliases.bash
@@ -2,7 +2,7 @@
 
 # Bundler Commands
 alias be="bundle exec"
-alias bi="bundle install"
+alias bi="bundle install --path vendor --binstubs=bin"
 alias bl="bundle list"
 alias bu="bundle update"
 alias bp="bundle package"
@@ -12,7 +12,7 @@ function bundler-help() {
   echo "Bundler Aliases Usage"
   echo
   echo "  be          = bundle exec"
-  echo "  bi          = bundle install"
+  echo "  bi          = bundle install --path vendor --binstubs=bin"
   echo "  bl          = bundle list"
   echo "  bu          = bundle update"
   echo "  bp          = bundle package"
